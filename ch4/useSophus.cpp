7542f4e5608f3c381b0c53d07c0113ea844ca3a6
Hao Li <leoli9901@gmail.com>
2022-03-08 21:09:22 +0800

    调整CMakeLists调用Sophus库的方式，使其能编译通过

--- ch4/useSophus.cpp
+++ ch4/useSophus.cpp
@@ -2,8 +2,7 @@
 #include <cmath>
 #include <Eigen/Core>
 #include <Eigen/Geometry>
-#include "sophus/se3.hpp"
-
+#include <sophus/se3.hpp>
 using namespace std;
 using namespace Eigen;
 
@@ -11,8 +10,8 @@ using namespace Eigen;
 
 int main(int argc, char **argv) {
 
-  // 沿Z轴转90度的旋转矩阵
-  Matrix3d R = AngleAxisd(M_PI / 2, Vector3d(0, 0, 1)).toRotationMatrix();
+  // 沿Z轴转45度的旋转矩阵
+  Matrix3d R = AngleAxisd(M_PI / 4, Vector3d(0, 0, 1)).toRotationMatrix();
   // 或者四元数
   Quaterniond q(R);
   Sophus::SO3d SO3_R(R);              // Sophus::SO3d可以直接从旋转矩阵构造
